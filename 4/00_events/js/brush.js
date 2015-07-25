f00fad742130619d2bcd7ac538eb95ed52b153bd
vizvamitra <vizvamitra@gmail.com>
2015-07-26 02:54:26 +0300

    4/00 improved line smootheness

--- 4/00_events/js/brush.js
+++ 4/00_events/js/brush.js
@@ -1,8 +1,9 @@
 window.Brush = function(){
   return {
     getVertices: function(start, end, color, size){
-      var dir = normalize(subtract(end, start));
-      var normal = scale(size/2, vec2(dir[1], -dir[0]));
+      // couldn't think out strightforward names for these vectors, sorry for that
+      var dir = scale(size/2, normalize(subtract(end, start)));
+      var normal = vec2(dir[1], -dir[0]);
 
       var temp = [
         add(start, normal), add(end, normal),
@@ -10,13 +11,17 @@ window.Brush = function(){
       ];
 
       var points = [
+        temp[0], temp[2], add(start, negate(dir)),
         start, temp[0], temp[1],
         start, temp[1], end,
         start, temp[3], end,
-        start, temp[2], temp[3]
+        start, temp[2], temp[3],
+        temp[1], temp[3], add(end, dir)
       ];
 
       var colors = [
+        color, color, color,
+        color, color, color,
         color, color, color,
         color, color, color,
         color, color, color,
